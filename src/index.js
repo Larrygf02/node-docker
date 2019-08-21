c12bf28ec76d0f2b6a3e218084b0a83799bf8808
larrygf02 <raulgf_02@hotmail.com>
2019-08-20 22:11:11 -0500

    Add route

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 const app = require('./app');
-
-app.listen(3000, () => console.log('Server on port 3000'))
+async function main() {
+    await app.listen(3000)
+    console.log('Server is running');
+}
+main()
+//app.listen(3000, () => console.log('Server on port 3000'))
